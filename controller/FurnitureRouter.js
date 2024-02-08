89a473252c4c068bb59f453068ceed7c5c74ffa8
MiniPaul <minipaul1227@gmail.com>
2024-02-09 01:05:28 +0530

    furniture-app-backend search branch created

--- controller/FurnitureRouter.js
+++ controller/FurnitureRouter.js
@@ -12,4 +12,10 @@ router.post("/add",async(req,res)=>{
     })
 })
 
+router.get("/search",async(req,res)=>{
+    let input = req.body
+    let data=await furnitureModel.find(input)
+    res.json(data)
+})
+
 module.exports=router
